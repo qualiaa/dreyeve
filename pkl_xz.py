b865a959ea34acd12ca0405a157eabbfabedf482
Jamie Bayne <jamiebayne@hotmail.com>
2018-05-11 17:53:13 +0100

    Update .pkl.xz compression code

--- pkl_xz.py
+++ pkl_xz.py
@@ -1,21 +1,21 @@
 import subprocess as proc
 import pickle as pkl
 
-def load_pkl_xz(input_path):
+def load(input_path):
     with open(input_path, "rb") as f:
         result = proc.run(["xz", "--decompress"], stdin=f, stdout=proc.PIPE)
         if result.returncode != 0:
-            raise ChildProcessError(input_path, result.returncode)
+            raise ChildProcessError(result.returncode)
         return pkl.loads(result.stdout)
 
-def save_pkl_xz(data, output_path):
+def save(data, output_path):
     with open(output_path, "wb") as datafile:
         pickle = pkl.dumps(data)
         result = proc.run(["xz"], input=pickle, stdout=datafile)
         if result.returncode != 0:
-            raise ChildProcessError(output_path, result.returncode)
+            raise ChildProcessError(result.returncode)
 
-def pkl_to_xz(file_path: str):
+def pkl_to_xz(file_path):
     with open(file_path, "rb") as in_file:
         with open(file_path + ".xz", "wb") as out_file:
             proc.run(["xz"], stdin=in_file, stdout=out_file)
