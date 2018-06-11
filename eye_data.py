59299c548f22bf4b0a28d9b037cf2df231bbaf4e
Jamie Bayne <jamiebayne@hotmail.com>
2018-06-11 19:12:15 +0100

    Improve comments and add label filtering parameter to read

--- eye_data.py
+++ eye_data.py
@@ -14,8 +14,12 @@ class Labels:
 LABEL_NAMES = { s:getattr(Labels,s) for s in Labels.__dict__.keys()
         if not s.startswith("_")}
 
-def read(video_folders):
-    """ Return an array of """
+def read(video_folders, desired_labels = [Labels.FIXATION]):
+    """ Return a list where each element is a table of eye data for a video.
+    Each table is a list of frames in the video, and contains a list of all
+    gaze-points recorded for that frame.
+    
+    Format is [video [frame np.array(y,x coords)]]. """
     eye_positions = list()
 
     for i, folder in enumerate(video_folders):
@@ -24,18 +28,26 @@ def read(video_folders):
             eye_data = csv.reader(f, delimiter=' ', dialect="unix")
             next(eye_data) # skip csv header
             for row in eye_data:
-                _, frame, x, y, label, _ = row
+                # extract relevant CSV data and convert to appropriate types
+                _, frame, x, y, label_name, _ = row
+
                 try:
-                    label_id = LABEL_NAMES[label.upper()]
+                    label = LABEL_NAMES[label_name.upper()]
                 except KeyError:
+                    # skip invalid labels
                     continue
+                
+                # filter undesired labels
+                if label not in desired_labels: continue
+
                 frame = int(frame)
                 coords = np.array([float(y), float(x)])
-                
-                if label_id != Labels.FIXATION: continue
+
+                # filter invalid gaze-points
                 if any(a > b for a, b in zip(coords,VIDEO_SHAPE)) or any(
                         a < 0 or math.isnan(a) for a in coords):
                     continue
+
                 #eye_pos[frame].append((label_id, coords))
                 eye_pos[frame].append(coords)
             eye_pos[frame]=np.array(eye_pos[frame])
@@ -46,12 +58,6 @@ def scale_to_shape(coords, target_shape):
     # calculate scale amount
     source_shape=VIDEO_SHAPE[0:2]
     if source_shape != target_shape:
-        #print("Input: {}".format(coords))
         scale=np.array([t/s for s, t in zip(source_shape, target_shape)])
-        #print("Scale: {}".format(scale))
-        #coords=[(y*scale[0], x*scale[1]) for y, x in coords]
         coords=coords*scale
-        """ print("Source shape: {}".format(source_shape))
-        print("Target shape: {}".format(target_shape))
-        print("Output: {}".format(coords)) """
     return coords
