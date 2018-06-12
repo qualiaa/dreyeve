97d392b173a0ea3067aaa5937ece5a2fdd8208a9
Jamie Bayne <jamiebayne@hotmail.com>
2018-06-12 13:51:26 +0100

    Fix stamp target, allow additive stamping

--- stamp.py
+++ stamp.py
@@ -1,17 +1,17 @@
 import numpy as np
 
-def stamp(array, coord, brush, out=None):
+def stamp(canvas, coord, brush, out=None, additive=True):
     if out is None:
-        out = np.array(array)
+        out = np.array(canvas)
     canvas = out
 
     centre = [int(round(a)) for a in coord]
     # top left and bottom right
     start_coords = [c - brush_shape//2 for c, brush_shape in zip(centre, brush.shape)]
-    end_coords = [c + brush_shape//2 for c, brush_shape in zip(centre, brush.shape)]
+    end_coords = [c + brush_shape//2 + 1 for c, brush_shape in zip(centre, brush.shape)]
 
     brush_start = [0,0]
-    brush_end = [e - 1 for e in brush.shape]
+    brush_end = brush.shape
 
     for i, start_coord in enumerate(start_coords):
         if start_coord < 0:
@@ -25,7 +25,11 @@ def stamp(array, coord, brush, out=None):
     stamp = brush[brush_start[0]:brush_end[0],
                   brush_start[1]:brush_end[1]]
 
-    canvas[start_coords[0]:end_coords[0],
-           start_coords[1]:end_coords[1]] = stamp
+    if additive:
+        canvas[start_coords[0]:end_coords[0],
+               start_coords[1]:end_coords[1]] += stamp
+    else:
+        canvas[start_coords[0]:end_coords[0],
+               start_coords[1]:end_coords[1]] = stamp
     return canvas
 
