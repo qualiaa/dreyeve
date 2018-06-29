5b38882ba073e8c1c0871c61e6173edd74c94752
Jamie Bayne <jamiebayne@hotmail.com>
2018-06-29 22:05:35 +0100

    Modify network to match details not found in paper

--- network.py
+++ network.py
@@ -5,6 +5,7 @@ from functools import reduce
 from keras import backend as K
 from keras.layers import Conv2D, Conv3D, Flatten, Input, Reshape, Lambda, concatenate
 from keras.layers import MaxPooling3D, UpSampling2D,UpSampling3D
+from keras.layers import BatchNormalization, Activation
 from keras.layers.advanced_activations import LeakyReLU
 from keras.models import Model
 import keras.utils.vis_utils as vis
@@ -25,15 +26,17 @@ def model(weights_file = None):
         tensor = examples[0]
         """
 
+    BN = BatchNormalization
     C3 = lambda filter_size: Conv3D(
             filter_size,
             (3, 3, 3),
             data_format=data_format,
             activation="relu",
             padding="same")
-    def P3(shape=(2, 2, 2)):
+    def P3(shape=(2, 2, 2),strides=None):
         return MaxPooling3D(
             shape,
+            strides=strides,
             data_format=data_format)
     C2 = lambda filter_size: Conv2D(
             filter_size,
@@ -44,28 +47,29 @@ def model(weights_file = None):
 
     coarse_architecture = [
         # encoder                        #112, 16
-        C3(64), P3(),                    #56 , 8
+        C3(64), P3((1,2,2)),               #56 , 8
         C3(128), P3(),                   #28 , 4
         C3(256), C3(256), P3(),          #14 , 2
-        C3(512), C3(512), P3(),          #7  , 1
+        C3(512), C3(512), P3(strides=(4,2,2)),          #7  , 1
         # decoder
         Reshape((512,7,7)),
-        C2(256), LeakyReLU(0.001), U2(), #14
-        C2(128), LeakyReLU(0.001), U2(), #28
-        C2(64),  LeakyReLU(0.001), U2(), #56
-        C2(32),  LeakyReLU(0.001), U2(), #112
-        C2(16),  LeakyReLU(0.001),
-        C2(1), LeakyReLU(0.001)
+        BN(), C2(256), LeakyReLU(0.001), U2(), #14
+        BN(), C2(128), LeakyReLU(0.001), U2(), #28
+        BN(), C2(64),  LeakyReLU(0.001), U2(), #56
+        BN(), C2(32),  LeakyReLU(0.001), U2(), #112
+        BN(), C2(16),  LeakyReLU(0.001),
+        BN(), C2(1), LeakyReLU(0.001)
     ]
 
     C2 = lambda f: Conv2D(f,(3,3),data_format=data_format,activation="relu",padding="same")
     fine_architecture = [
         C2(32),
-        C2(64),
-        C2(32),
-        C2(32),
-        C2(16),
-        C2(1)
+        C2(64), LeakyReLU(0.001),
+        C2(32), LeakyReLU(0.001),
+        C2(32), LeakyReLU(0.001),
+        C2(16), LeakyReLU(0.001),
+        C2(4), LeakyReLU(0.001),
+        C2(1), Activation("relu")
     ]
 
     def coarse_inference(x):
