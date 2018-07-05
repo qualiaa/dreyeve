4cafc96b9cd3436a99ac45eebb32ab7cc2936a04
Jamie Bayne <jamiebayne@hotmail.com>
2018-07-05 16:25:39 +0100

    Fix plot subaxes

--- analysis.py
+++ analysis.py
@@ -36,20 +36,21 @@ files = glob("history*pkl.xz")
 
 for f in files:
     match = re.fullmatch("history_(\d)_(\d).pkl.xz",f)
-    radius, frames = match.groups()
+    radius, frames = [int(x) for x in match.groups()]
     hist = pkl_xz.load(f)
 
     train.gaze_radius = radius
-    train.gaze_frames = hist
+    train.gaze_frames = frames
     val.gaze_radius = radius
-    val.gaze_frames = hist
+    val.gaze_frames = frames
 
-    model = network.model("weights_{:d}_{:d}.h5".format(radius,hist))
+    model = network.model("weights_{:d}_{:d}.h5".format(radius,frames))
 
     X_train, Y_true = train.get_example(0)
+    X_train = [X[None,:] for X in X_train]
     X_train_c, X_train_f, _ = X_train
     Y_true_c, Y_true_f = Y_true
-    Y_pred_c, Y_pred_f = model.predict([X],batch_size=1)
+    Y_pred_c, Y_pred_f = model.predict(X_train,batch_size=1)
     kl_c = kl_vis(Y_true_c, Y_pred_c)
     kl_f = kl_vis(Y_true_f, Y_pred_f)
     cc_c = cc_vis(Y_true_c, Y_pred_c)
@@ -57,35 +58,31 @@ for f in files:
 
     fig = plt.figure()
 
-    ax = fig.subplot(221)
-    subax = ax.subplot(211)
-    subax.imshow(X_train_c)
-    subax = ax.subplot(212)
-    subax.imshow(X_train_f)
+    ax1 = plt.subplot2grid((4, 4), (0, 0), colspan=2)
+    ax1.imshow(X_train_c)
+    ax2 = plt.subplot2grid((4, 4), (1, 0), colspan=2)
+    ax2.imshow(X_train_f)
 
-    ax = fig.subplot(222)
-    subax = ax.subplot(221)
-    subax.imshow(Y_true_c)
-    subax = ax.subplot(222)
-    subax.imshow(Y_true_f)
-    subax = ax.subplot(223)
-    subax.imshow(Y_pred_c)
-    subax = ax.subplot(224)
-    subax.imshow(Y_pred_f)
+    ax3 = plt.subplot2grid((4, 4), (0, 2))
+    ax3.imshow(Y_true_c)
+    ax4 = plt.subplot2grid((4, 4), (0, 3))
+    ax4.imshow(Y_true_f)
+    ax5 = plt.subplot2grid((4, 4), (1, 2))
+    ax5.imshow(Y_pred_c)
+    ax6 = plt.subplot2grid((4, 4), (1, 3))
+    ax6.imshow(Y_pred_f)
 
-    ax = fig.subplot(223)
-    subax = ax.subplot(211)
-    subax.imshow(kl_c)
-    subax = ax.subplot(212)
-    subax.imshow(kl_f)
+    ax7 = plt.subplot2grid((4, 4), (2, 0), colspan=2)
+    ax7.imshow(kl_c)
+    ax8 = plt.subplot2grid((4, 4), (3, 0), colspan=2)
+    ax8.imshow(kl_f)
 
-    ax = fig.subplot(224)
-    subax = ax.subplot(211)
-    subax.imshow(cc_c)
-    subax = ax.subplot(212)
-    subax.imshow(cc_f)
+    ax9 = plt.subplot2grid((4, 4), (2, 2), colspan=2)
+    ax9.imshow(cc_c)
+    ax10 = plt.subplot2grid((4, 4), (3, 2), colspan=2)
+    ax10.imshow(cc_f)
 
-    plt.show()
+    fig.show()
 
 
 
