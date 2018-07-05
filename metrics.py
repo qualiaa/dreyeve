c1eb753c566476c537377b52c5e1a3d922e12d8f
Jamie Bayne <jamiebayne@hotmail.com>
2018-07-05 22:42:06 +0100

    Replace tf with np for metric visualisation code

--- metrics.py
+++ metrics.py
@@ -12,10 +12,10 @@ def cross_correlation(y_true, y_pred):
     return K.mean(K.dot(y_true - K.mean(y_true), y_pred - K.mean(y_pred)))
 
 def kl_vis(y_true, y_pred):
-    eps = K.epsilon() # 1.19e-07
+    eps = 1.19e-07
     denom = (eps + y_true)
     ratio = y_pred/denom
-    return y_pred * K.log(eps + ratio)
+    return y_pred * np.log(eps + ratio)
 
 def cc_vis(y_true, y_pred):
     return y_true * y_pred / np.sqrt(np.sum(y_true**2 + y_pred**2))
