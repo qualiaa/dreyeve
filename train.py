74c184ab0dba4ccb163e1ffcaa305d8f0358f6c9
Jamie Bayne <jamiebayne@hotmail.com>
2018-07-12 18:07:14 +0100

    Add ArgumentParser for selecting loss fn

--- train.py
+++ train.py
@@ -1,27 +1,45 @@
 #!/usr/bin/env python3
 
-import sys
+import shutil
 from glob import glob
+from pathlib import Path
 
 import numpy as np
 import tensorflow as tf
-import warnings
 
 from keras.callbacks import TerminateOnNaN, ModelCheckpoint, TensorBoard
 
 import network
+import metrics
 import consts as c
 import utils.pkl_xz as pkl_xz
 from utils.Examples import KerasSequenceWrapper
 from DreyeveExamples import DreyeveExamples
 
-def train(gaze_radius = 16, gaze_frames = 16):
-    #warnings.filterwarnings("ignore")
+loss_metrics = {
+    "kl": metrics.kl_divergence,
+    "cc": metrics.cross_correlation,
+    "mse": "mse"
+}
+
+def train(gaze_settings=(16,16), loss_name="kl"):
+    gaze_radius, gaze_frames = gaze_settings
+    loss = loss_metrics[loss_name]
+    run_name = "{}_{:d}_{:d}".format(loss_name, gaze_radius, gaze_frames)
+
+    tb_path = Path(c.TB_DIR, run_name)
+    if tb_path.exists():
+        print("TensorBoard logdir", tb_path, "already exists. Overwrite [y/N]")
+        r = input()
+        if r.lower()[0] == "y":
+            shutil.rmtree(str(tb_path))
+        else:
+            return
 
     print("Loading model...")
     model = network.model()
     opts = tf.RunOptions(report_tensor_allocations_upon_oom=True)
-    model.compile(optimizer='adam',loss='mse',options=opts)
+    model.compile(optimizer='adam',loss=loss, options=opts)
 
     video_folders = glob(c.DATA_DIR + "/[0-9][0-9]")
 
@@ -44,13 +62,12 @@ def train(gaze_radius = 16, gaze_frames = 16):
         TerminateOnNaN(),
         ModelCheckpoint(
             c.CHECKPOINT_DIR + "/" +
-                "{:d}_{:d}".format(gaze_radius,gaze_frames) +
+                run_name + 
                 "_epoch_{epoch:02d}_loss_{val_loss:.2f}.h5",
             save_weights_only=True,
             period=2),
         TensorBoard(
-            log_dir=c.TB_DIR+
-                "/{:d}_{:d}".format(gaze_radius,gaze_frames),
+            log_dir=str(tb_path),
             #histogram_freq=10,
             batch_size=c.BATCH_SIZE,write_images=True,
             write_graph=True)
@@ -67,22 +84,30 @@ def train(gaze_radius = 16, gaze_frames = 16):
 
 
     print("Saving weights and history")
-    model.save_weights("weights_{:d}_{:d}.h5".format(gaze_radius,gaze_frames))
-    pkl_xz.save(history.history,"history_{:d}_{:d}.pkl.xz".format(gaze_radius,gaze_frames))
+    model.save_weights("weights_" + run_name + ".h5")
+    pkl_xz.save(history.history,"history_" + run_name + ".pkl.xz")
 
 if __name__ == "__main__":
     import os
+    import warnings
+    import argparse
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+    warnings.filterwarnings("ignore")
 
-    nargs = len(sys.argv) - 1
-    if nargs % 2 == 1 or nargs == 0:
-        sys.stderr.write(
-            "Must provide [[gaze radius], [gaze_frames], ...] as arguments\n")
-        sys.exit(1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--loss",default="kl")
 
-    args = [int(arg) for arg in sys.argv[1:]]
-    args = zip(args[::2],args[1::2])
+    def extract_settings(arg):
+        result = tuple(int(x) for x in arg.split(","))
+        if len(result) != 2:
+            raise argparse.ArgumentError()
+        return result
 
-    for radius, frames in args:
-        print(radius,frames)
-        train(int(radius), int(frames))
+    parser.add_argument("gaze_settings",nargs="+",type=extract_settings)
+    args = vars(parser.parse_args())
+    loss = args["loss"]
+    gaze_settings_list = args["gaze_settings"]
+
+    for gaze_settings in gaze_settings_list:
+        print(gaze_settings)
+        train(gaze_settings,loss)
