c1a718dfd62f23de221bb7fdf61be996c66a74ea
Jamie Bayne <jamiebayne@hotmail.com>
2018-07-17 19:08:14 +0100

    Rewrite for clarity and fix kl_vis

--- metrics.py
+++ metrics.py
@@ -4,28 +4,34 @@ import numpy as np
 import consts as c
 
 def _normalize(x):
-    x_min = K.min(x, axis=[1,2], keepdims=True)
-    # this line isn't necessary if last layer is a ReLU
+    # these two lines aren't necessary if last layer is a ReLU
+    #x_min = K.min(x, axis=[1,2], keepdims=True)
     #x -= (x_min - K.abs(x_min)) / 2
     x /= K.sum(x, axis=[1,2], keepdims=True)
     return x
 
 def kl_divergence(y_true, y_pred):
-    y_pred = _normalize(y_pred)
+    Q = y_true; P = _normalize(y_pred)
     eps = K.epsilon() # 1.19e-07
-    denom = (eps + y_pred)
-    ratio = y_true/denom
-    return K.sum(K.tf.multiply(y_true,K.log(eps + ratio)))
+    return K.sum(K.tf.multiply(Q, K.log(eps + Q/(eps + P))), axis=[1,2])
 
 
 def cross_correlation(y_true, y_pred):
-    return K.mean(K.dot(y_true - K.mean(y_true), y_pred - K.mean(y_pred)))
+    X = y_true; Y = y_pred
+    X_stddev = K.std(X, axis=[1,2])
+    Y_stddev = K.std(Y, axis=[1,2])
+    X_mean = K.mean(X, axis=[1,2])
+    Y_mean = K.mean(Y, axis=[1,2])
+
+    denom = K.tf.multiply(X_stddev,Y_stddev)
+
+    return K.mean(K.dot(X-X_mean, Y-Y_mean),axis=[1,2]) / denom
 
 def kl_vis(y_true, y_pred):
     eps = c.EPS
-    denom = (eps + y_true)
-    ratio = y_pred/denom
-    return y_pred * np.log(eps + ratio)
+    Q = y_true
+    P = y_pred
+    return y_pred * np.log(eps + Q/(eps + P))
 
 def cc_vis(y_true, y_pred):
     return y_true * y_pred / np.sqrt(np.sum(y_true**2 + y_pred**2))
